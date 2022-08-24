4fe803f562cd2ddf7465d9e566aa381310691ab5
Diana <Shimlykhdiana@gmail.com>
2022-08-24 22:20:44 +0300

    add files

--- task3/script.js
+++ task3/script.js
@@ -5,26 +5,33 @@ const allTrafficLights = document.querySelector('#allTrafficLights');
 
 function makeRed() {
     trafficLightEl3.style.background = ('red');
-    trafficLightEl3.removeEventListener('click', makeRed);
-    trafficLightEl3.addEventListener('click', makeGreen);
+    allTrafficLights.removeEventListener('click', makeRed);
+    allTrafficLights.addEventListener('click', makeGreen);
+    trafficLightEl2.addEventListener('click', makeBlack);
+    trafficLightEl1.addEventListener('click', makeBlack);
 }
-
-trafficLightEl3.addEventListener('click', makeRed);
 
 function makeYellow() {
     trafficLightEl2.style.background = ('yellow');
-    trafficLightEl2.removeEventListener('click', makeYellow);
-    trafficLightEl2.addEventListener('click', makeRed);
+    allTrafficLights.removeEventListener('click', makeYellow);
+    allTrafficLights.addEventListener('click', makeRed);
+    trafficLightEl1.addEventListener('click', makeBlack);
+    trafficLightEl3.addEventListener('click', makeBlack);
 }
 
-trafficLightEl2.addEventListener('click', makeYellow);
+
 
 function makeGreen() {
     trafficLightEl1.style.background = ('green');
-    trafficLightEl1.removeEventListener('click', makeGreen);
-    trafficLightEl1.addEventListener('click', makeYellow);
+    allTrafficLights.removeEventListener('click', makeGreen);
+    allTrafficLights.addEventListener('click', makeYellow);
+    trafficLightEl3.addEventListener('click', makeBlack);
+    trafficLightEl2.addEventListener('click', makeBlack);
 }
 
 trafficLightEl1.addEventListener('click', makeGreen);
 
 
+function makeBlack () {
+   this.style.background = ('black');
+}
